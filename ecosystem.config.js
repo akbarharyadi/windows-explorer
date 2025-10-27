c977732b9b40534515dd2f8bbf03f24faf3e43b3
Akbar Priyono Haryadi <akbarharyadi@outlook.com>
2025-10-27 19:29:58 +0700

    deploy to server

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: 'window-explorer-backend',
+      script: './packages/backend/dist/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DATABASE_URL:
+          'postgresql://window-explorer:window-explorer_password@localhost:5432/window-explorer_db?schema=public',
+        REDIS_URL: 'redis://localhost:6379',
+        RABBITMQ_URL: 'amqp://window-explorer:window-explorer_password@localhost:5672',
+        SERVICE_ROLE: 'api',
+      },
+    },
+    {
+      name: 'window-explorer-worker',
+      script: './packages/worker/dist/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        DATABASE_URL:
+          'postgresql://window-explorer:window-explorer_password@localhost:5432/window-explorer_db?schema=public',
+        REDIS_URL: 'redis://localhost:6379',
+        RABBITMQ_URL: 'amqp://window-explorer:window-explorer_password@localhost:5672',
+        SERVICE_ROLE: 'worker',
+      },
+    },
+  ],
+}
